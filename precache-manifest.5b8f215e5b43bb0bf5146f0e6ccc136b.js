2c3b983448cfa922740eb093dc7cfd392265b25e
HanyMagdy25 <hany.magdy25@gmail.com>
2021-12-01 04:32:22 +0200

    Updates

---
+++ precache-manifest.5b8f215e5b43bb0bf5146f0e6ccc136b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "263164b380a415cf9b1cd118047a4c7c",
+    "url": "/art/index.html"
+  },
+  {
+    "revision": "98ab1b2a43d079b7e3a3",
+    "url": "/art/static/css/main.8349d4be.chunk.css"
+  },
+  {
+    "revision": "ed9d0aa0edc20adc02af",
+    "url": "/art/static/js/2.05fe5eca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/art/static/js/2.05fe5eca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98ab1b2a43d079b7e3a3",
+    "url": "/art/static/js/main.1ca53ee1.chunk.js"
+  },
+  {
+    "revision": "affa822f6b032eb26eda",
+    "url": "/art/static/js/runtime-main.33348910.js"
+  }
+]);
